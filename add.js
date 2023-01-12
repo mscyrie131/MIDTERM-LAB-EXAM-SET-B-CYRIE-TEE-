741d2a56e24f3773eb3f6e70c0d427907d840fba
Cyrie Hannanael G. Tee <teecyrie@gmail.com>
2023-01-12 19:50:38 +0800

    init

---
+++ add.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+
+const add = function(note, oldNote) {
+    const newNote = JSON.parse(oldNote);
+// add newNote to oldNote
+    newNote.push(note);
+
+    fs.writeFileSync('data.txt', JSON.stringify(newNote));
+}
+module.exports = add
---
+++ read.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const read = function() {
+        if(fs.existsSync('./data.txt')) {
+        return fs.readFileSync('data.txt', 'utf8');
+
+} else {
+        return '[]';
+        }
+}
+
+module.exports = read
